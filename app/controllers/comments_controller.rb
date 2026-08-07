aefef4d1bd4dcf0aa0a7a5055787674e5a113a82
omatakugayama-glitch <omatakugayama@gmail.com>
2026-08-07 19:45:04 +0900

    comment controllerの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
+      @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show", status: :unprocessable_entity
     end
